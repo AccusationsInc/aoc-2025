9244c184b71c4c34e764d17ac8da25b4d07d8a94
AccusationsInc <nathan.herr445@gmail.com>
2025-12-01 02:51:17 -0600

    Generated first answer, incorrect

---
+++ Day One/dayone.py
@@ -0,0 +1,41 @@
+#Set file path
+filename = "input.txt"
+
+#Read input into an array
+with open(filename) as file:
+    lines = [line.rstrip() for line in file]
+
+#set count to 0
+password = 0
+
+#Set current dial position
+currentPos = 50
+
+#Iterate through each line
+for rotation in lines :
+
+    print(f"Starting Position :: {currentPos}")
+    print("Rotation :: " + rotation)
+    #Direction to turn dial
+    direction = rotation[0]
+
+    #Clicks to turn dial
+    clicks = int(rotation[1:])
+    
+    if(direction == "L") :
+        clicks *= -1
+    
+    currentPos += clicks
+
+    print("testing :: " + str(currentPos))
+    if(currentPos > 99) :
+        currentPos = (currentPos % 99) - 1
+    elif (currentPos < 0) :
+        currentPos = 100 + currentPos
+    
+    if (currentPos == 0) :
+        password += 1
+    
+    print(f"Ending Position :: {currentPos}\n\n")
+
+print("This is the password :: " + str(password))
